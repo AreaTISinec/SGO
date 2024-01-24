bf2411109adf3af6fc146c8d22feeafecb4356f2
LDgtt <lksdro01@gmail.com>
2024-01-24 10:23:20 -0300

    vista cene, url api all

--- backend/cene/urls.py
+++ backend/cene/urls.py
@@ -1,1 +1,6 @@
 from django.urls import path
+from .views import CeneListView
+
+urlpatterns = [
+    path('', CeneListView.as_view()),
+]
--- backend/cene/views.py
+++ backend/cene/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework import permissions
 from .models import Cene
 from .serializers import CeneSerializer
@@ -8,4 +8,15 @@ class CeneListView(ListAPIView):
     permission_classes = (permissions.AllowAny, )
     queryset = Cene.objects.all()
     serializer_class = CeneSerializer
+
+class CeneItemView(RetrieveAPIView):
+    permission_classes = (permissions.AllowAny, )
+    serializer_class = CeneSerializer
     
+    def get_queryset(self):
+        param = self.kwargs['param']
+        
+        queryset1 = Cene.objects.filter(id_cene=param)
+        queryset2 = Cene.objects.filter(nombre = param)
+        
+        return queryset1
