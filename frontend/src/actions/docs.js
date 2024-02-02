b9be909602a37d670eaad36378e6b0b6fca24347
LDgtt <lksdro01@gmail.com>
2024-02-02 14:09:33 -0300

    alo

--- frontend/src/actions/docs.js
+++ frontend/src/actions/docs.js
@@ -17,7 +17,7 @@ export const upload = async (doc, nombre, tipo, id) => {
     try {
       const res = await axios.post('http://127.0.0.1:8000/api/docs/upload/', formData, config)
       console.log(formData)
-
+sdas
 
   }catch(err){
     console.error(err)
