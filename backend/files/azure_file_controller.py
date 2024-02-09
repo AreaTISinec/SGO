da40c9c32e58cd6d2d9198710238d428dee21bb6
LDgtt <lksdro01@gmail.com>
2024-02-09 15:23:14 -0300

    conexion de los archivos con su obra correspondiente

--- backend/files/azure_file_controller.py
+++ backend/files/azure_file_controller.py
@@ -9,6 +9,7 @@ from azure.storage.blob import BlobClient, BlobServiceClient
 from django.conf import settings
 
 from .models import File
+from obras.models import Obras
 
 
 ALLOWED_EXTENTIONS = ['.pdf', '.doc', '.docx', '.xlsx']
@@ -22,20 +23,21 @@ def create_blob_client(file_name):
     blob_client = blob_service_client.get_blob_client(container=container_name, blob=file_name)
     return blob_client
 
-def save_file_url_to_db(file_url):
-    new_file = File.objects.create(file_url=file_url)
+def save_file_url_to_db(file_url, tipo, id_obra):
+    new_file = File.objects.create(file_url=file_url, tipo=tipo, id_obra=id_obra)
     new_file.save()
     return new_file
     
-def upload_file_to_blob(file):
-    prefix = 'nombrewan'
+def upload_file_to_blob(file, tipo, id_obra):
+    obra = Obras.objects.get(pk=id_obra)
+    
+    prefix = uuid.uuid4().hex
     ext = Path(file.name).suffix
     blob_name = f'{prefix}{ext}'
-    print(blob_name)
     file_io = BytesIO(file.read())
     blob_client = create_blob_client(blob_name)
     blob_client.upload_blob(data=file_io)
-    file_object = save_file_url_to_db(blob_client.url)
+    file_object = save_file_url_to_db(blob_client.url, tipo, obra)
     return file_object
     
     
