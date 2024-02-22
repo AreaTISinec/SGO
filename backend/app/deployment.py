5c4102a8ac225548722fd92e271efd36ca983292
optic4l <lksdro01@gmail.com>
2024-02-22 14:02:33 -0300

    static root

--- backend/app/deployment.py
+++ backend/app/deployment.py
@@ -25,7 +25,7 @@ MIDDLEWARE = [
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'dist/assets')
 
 DATABASES = {
     'default': {
