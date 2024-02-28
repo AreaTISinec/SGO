00389c1665a7cae19f238fc42c29dc401302148b
tomvssss <tomas.incubus16@gmail.com>
2024-02-28 15:13:16 -0300

    incorporacion de emergentes correcto/error para nueva obra y nuevo cene

--- frontend/src/actions/newWorks.js
+++ frontend/src/actions/newWorks.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-
+import swal from 'sweetalert2';
 
 export const uploadObra = async (fecha_inicio, fecha_termino, fecha_asignacion, monto_neto, empresa, direccion, comuna, tipo_obra, estado_obra, observaciones, porc_avance, monto_facturado, saldo_facturado, id_user, id_cene) => {
     console.log('holamundo')
@@ -30,9 +30,27 @@ export const uploadObra = async (fecha_inicio, fecha_termino, fecha_asignacion, 
 
     try {
         const res = await axios.post('http://127.0.0.1:8000/api/obras/nueva/', body, config)
+        swal.fire({
+            title: "Nueva obra creada exitosamente",
+            icon: "success",
+            toast: true,
+            timer: 4000,
+            position: 'top-right',
+            timerProgressBar: true,
+            showConfirmButton: false,
+        })
         return res
     }catch(err){
         console.error(err)
+        swal.fire({
+            title: "Error al crear la obra",
+            icon: "error",
+            toast: true,
+            timer: 4000,
+            position: 'top-right',
+            timerProgressBar: true,
+            showConfirmButton: false,
+          })
         throw err
     }
 
