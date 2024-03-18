39fc4a9e0d87c84cbdf1c6f80a5f959926c8b909
optic4l <lksdro01@gmail.com>
2024-03-18 17:07:20 -0300

    se limpio el detalle de obras

--- frontend/src/components/Obras/DetalleObra.jsx
+++ frontend/src/components/Obras/DetalleObra.jsx
@@ -1,7 +1,5 @@
 import { useParams, Link } from "react-router-dom";
 import { useState, useEffect } from 'react';
-import { PowerBIEmbed } from 'powerbi-client-react';
-import { models } from 'powerbi-client';
 import SidebarV2 from "../SidebarV2/SidebarV2";
 import Button from "react-bootstrap/Button";
 import Modal from 'react-bootstrap/Modal';
@@ -12,11 +10,8 @@ import { uploadAvanceReal } from "../../actions/newAvance";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
 import Divider from '@mui/material/Divider';
-import { getAccessToken, getDetalleObra, getEncargado, getSupervisores } from "../../actions/getPetitions.js"
+import { getDetalleObra, getEncargado, getSupervisores } from "../../actions/getPetitions.js"
 import Spinner from 'react-bootstrap/Spinner';
-
-
-
 
 
 const DetalleObra = () => {
@@ -25,17 +20,10 @@ const DetalleObra = () => {
   const [supervisor, setSupervisor] = useState({});
   const [responsable, setResponsable] = useState({});
   const [showAR, setShowAR] = useState(false);
-  const [supervisores, setSupervisores] = useState([])
-  // const [showAP, setShowAP] = useState(false);
-  // const [numHitos, setNumHitos] = useState(0);
-  const [fechaActual, setFechaActual] = useState('')
-  // const [avances, setAvances] = useState([])
-  // const [hitos, setHitos] = useState([{
-  // }])
-  // const [errores, setErrores] = useState([]);
-  // const [avanceProyectado, setAvanceProyectado] = useState([])
+  const [supervisores, setSupervisores] = useState([]);
+  const [fechaActual, setFechaActual] = useState('');
 
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
 
   const handleClick = () => {
     setLoading(true)
@@ -46,9 +34,6 @@ const DetalleObra = () => {
 
   const handleCloseAR = () => setShowAR(false);
   const handleShowAR = () => setShowAR(true);
-
-  // const handleCloseAP = () => setShowAP(false);
-  // const handleShowAP = () => setShowAP(true);
 
   const {  porcentaje, onInputChange, onResetForm } = useForm({
     porcentaje: 0
@@ -70,80 +55,13 @@ const DetalleObra = () => {
     handleCloseAR()
   }
  
-  // const onChangeProyectado = (e, index) => {
-  //   const { name, value } = e.target;
   
-  //   setHitos(prevState => {
-  //     const nuevosHitos = [...prevState];
-  //     nuevosHitos[index] = {
-  //       ...nuevosHitos[index],
-  //       [name]: value
-  //     };
-  
-  //     // Creamos una variable para almacenar el mensaje de error
-  //     let errorMessage = '';
-  
-  //     // Validamos el campo 'fecha'
-  //     if (name === 'fecha') {
-  //       if(nuevosHitos[index - 1] && nuevosHitos[index - 1].fecha >= value)
-  //         errorMessage = `Ingrese una fecha mayor a ${nuevosHitos[index-1].fecha}`;
-        
-  //       if(nuevosHitos[index + 1] && nuevosHitos[index + 1].fecha <= value)
-  //         errorMessage = `Ingrese una fecha menor a ${nuevosHitos[index+1].fecha}`;
-  //     }
-  
-  //     // Validamos el campo 'porcentaje'
-  //     if (name === 'porcentaje') {
-  //       if(nuevosHitos[index - 1] && parseInt(nuevosHitos[index - 1].porcentaje) >= value)
-  //         {errorMessage = `Ingrese un porcentaje mayor a ${nuevosHitos[index-1].porcentaje}`;}
-
-  //       if(nuevosHitos[index + 1] && parseInt(nuevosHitos[index + 1].porcentaje) <= value)
-  //         errorMessage = `Ingrese un porcentaje menor a ${nuevosHitos[index+1].porcentaje}`;
-
-  //       if(nuevosHitos[numHitos] && parseInt(nuevosHitos[numHitos].porcentaje) !== 100)
-  //         errorMessage = 'Su ultimo porcentaje debe ser igual a 100';
-  //     }
-  
-  //     // Actualizamos el estado de errores
-  //     setErrores(prevErrors => {
-  //       const newErrors = [...prevErrors];
-  //       newErrors[index] = {name: name, message: errorMessage}
-  //       return newErrors;
-  //     });
-      
-
-  //     return nuevosHitos;
-  //   });
-  // };
-
-
-  // const avanceProyecSubmit = (e) => {
-  //   e.preventDefault();
-
-  //   uploadAvanceProyectado(hitos, idObra);
-      
-  //   onResetForm()
-  //   handleCloseAR()
-  // }
-
-
-  const [accessToken, setAccessToken] = useState('');
-
-  useEffect(() => {
-    getAccessToken(setAccessToken)
-    //   //Refresca el token de acceso cada hora (3600000 milisegundos)
-    //  const intervalId = setInterval(fetchAccessToken, 3600000);
-
-    //   //Limpia el intervalo cuando el componente se desmonta
-    //  return () => clearInterval(intervalId);
-   }, []);  //Ejecutar efecto solo en el montaje inicial del componente
 
 
   useEffect(() => {
     getDetalleObra(idObra, setDetalleObra)
     const fecha = new Date().toISOString().split('T')[0];
     setFechaActual(fecha)
-    // getAvances(idObra, setAvances)
   }, [idObra]); // Ejecutar efecto solo en el montaje inicial del componente
 
   useEffect(()=>{
@@ -152,48 +70,6 @@ const DetalleObra = () => {
     getSupervisores(setSupervisores)
   }, [detalleObra])
 
- 
-
-// const renderHitosFields = () => {
-//   const fields = [];
-//   for (let i = 1; i <= numHitos; i++) {
-//     fields.push(
-//       <div  key={i}>
-
-//         <Form.Group>
-//           <Form.Label>{`Fecha hito ${i}`}</Form.Label>
-//           <Form.Control 
-//             type="date"
-//             name={'fecha'}
-//             placeholder={`Ingrese la fecha del hito ${i}`}
-//             onChange={ (e) => {
-//               onChangeProyectado(e, i)
-//             }}
-//             required
-//           />
-//           {errores[i] && errores[i].name === 'fecha' && <span style={{ color: 'red' }}>{errores[i].message}<br/></span>}
-//           <Form.Label><br/>{`% Avance esperado hito ${i}`}</Form.Label>
-//           <Form.Control 
-//             type="number"
-//             name={'porcentaje'}
-//             placeholder={`Ingrese el porcentaje de avance del hito ${i}`}
-//             onChange={(e) => {
-//               onChangeProyectado(e, i)
-//             }}
-//             required
-//             max={100}
-//             min={0}
-//           />
-//           {errores[i] && errores[i].name === 'porcentaje' && <span style={{ color: 'red' }}>{errores[i].message}</span>}
-//         </Form.Group>
-//         <br/><br/>
-//       </div>
-//     );
-//   }
-//   return fields;
-// };
-
- 
 
   return (
     <div className="DetalleObraContainer">
@@ -328,42 +204,6 @@ const DetalleObra = () => {
                 <div className="divider"><Divider/></div>
                 <div className="Dato obs"><strong>Observaciones:</strong><span className="value-dato">{detalleObra.observaciones}</span></div>
 
-              </div>
-              <div className="reportClass">
-
-                <PowerBIEmbed
-                embedConfig = {{
-                  type: 'report',   // Supported types: report, dashboard, tile, visual, qna, paginated report and create
-                  id: '5c607318-8d82-49bf-a371-7e0edf855485',
-                  embedUrl: 'https://app.powerbi.com/reportEmbed?reportId=5c607318-8d82-49bf-a371-7e0edf855485&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly9XQUJJLVBBQVMtMS1TQ1VTLXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0IiwiZW1iZWRGZWF0dXJlcyI6eyJ1c2FnZU1ldHJpY3NWTmV4dCI6dHJ1ZSwiZGlzYWJsZUFuZ3VsYXJKU0Jvb3RzdHJhcFJlcG9ydEVtYmVkIjp0cnVlfX0%3d',
-                  accessToken: accessToken.access_token,
-                  tokenType: models.TokenType.Aad, // Use models.TokenType.Aad for SaaS embed
-                  settings: {
-                    panes: {
-                      filters: {
-                        expanded: false,
-                        visible: false
-                      }
-                    },
-                    background: models.BackgroundType.Transparent,
-                  }
-                }}
-
-                eventHandlers = {
-                  new Map([
-                    ['loaded', function () {console.log('Report loaded');}],
-                    ['rendered', function () {console.log('Report rendered');}],
-                    ['error', function (event) {console.log(event.detail);}],
-                    ['visualClicked', () => console.log('visual clicked')],
-                    ['pageChanged', (event) => console.log(event)],
-                  ])
-                }
-                
-                cssClassName = { "reportClass" }
-                getEmbeddedComponent = { (embeddedReport) => {
-                  window.report = embeddedReport ;
-                }}
-                />
               </div>
             </div>
           )}
