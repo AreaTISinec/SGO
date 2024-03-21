ca38956bff386ebe2cdc31facde181db9faf61c8
optic4l <lksdro01@gmail.com>
2024-03-21 17:42:31 -0300

    se creo vista para modulo de facturacion

---
+++ frontend/src/containers/Facturacion/Facturacion.jsx
@@ -0,0 +1,15 @@
+import SidebarV2 from "../../components/SidebarV2/SidebarV2"
+import './Facturacion.css'
+
+const Facturacion = () => {
+  return (
+    <div className="facturacion-container">
+    <SidebarV2 />
+      <div className="recuadro-contenido">
+        VISTA FACTURACION
+      </div>
+    </div>
+  )
+}
+
+export default Facturacion
